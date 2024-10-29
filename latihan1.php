bb3e1b4847a5c40e80891373fbec27fa3dbaee78
AziziGilang25 <muhammadazk41@gmail.com>
2024-10-29 16:21:52 +0700

    Add files via upload
    Hallo

---
+++ latihan1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "selamat sore guys...!<br>
+    <b><font color ='red'>Nama : Azizi Gilang </font></b><br>
+    <b>NPM : 23753090 </b><br>
+    <b>Jurusan : Tekhnologi Informasi</b><br>
+    <u>Prodi : MI</u>";
+     
+  
+    
+    ?>
+
+</body>
+</html>
